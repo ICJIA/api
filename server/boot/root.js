261be9670726a80d7df6ad6a6acd0ff78ad15ddd
Chris Schweda <cschweda@gmail.com>
2017-07-16 16:08:25 +0000

    Add custom root page

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,22 @@
 'use strict';
+var path = require('path');
 
 module.exports = function(server) {
   // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+ // var router = server.loopback.Router();
+  //router.get('/', server.loopback.status());
+  //server.use(router);
+
+
+var router = server.loopback.Router();
+  router.get('/status', server.loopback.status());
+  router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../../client/index.html'));
+  });
   server.use(router);
+
+
+
+
+
 };
