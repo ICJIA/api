a07597109457a7f4c137a176578bda609577f9e2
Chris Schweda <cschweda@gmail.com>
2017-07-16 14:18:34 +0000

    Update permalink

--- common/models/permalink.js
+++ common/models/permalink.js
@@ -2,4 +2,32 @@
 
 module.exports = function(Permalink) {
 
+ var validateUUID = require('uuid-validate');
+
+  // mdContent must be at least 3 characters
+  Permalink.validatesLengthOf('mdContent', {
+    min: 3,
+    message: {
+      min: 'Markdown content must be at least 3 characters.'
+    }
+  });
+
+  //Custom validate UUID for insertion into database
+  const validatesUUID = function (err) {
+    if (!validateUUID(this.permalink)) {
+      err()
+    }
+  }
+
+  Permalink.validate('permalink',validatesUUID, {
+    message: 'Permalink must be a valid UUID'
+  })
+
+  // permalink must be unique
+  Permalink..validatesUniquenessOf('permalink')
+
+
+
+
+
 };
