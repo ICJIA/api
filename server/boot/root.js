57b7c393921fd8f6498862177253d0a805ebb23c
cschweda <cschweda@gmail.com>
2017-07-17 08:57:54 -0500

    Tweak static client html

--- server/boot/root.js
+++ server/boot/root.js
@@ -10,9 +10,9 @@ module.exports = function(server) {
 
 var router = server.loopback.Router();
   router.get('/status', server.loopback.status());
-  router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/../../client/index.html'));
-  });
+  // router.get('/', function(req, res) {
+  //   res.sendFile(path.join(__dirname + '/../../client/index.html'));
+  // });
   server.use(router);
 
 
